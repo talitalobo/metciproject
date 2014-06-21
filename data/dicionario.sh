2ebe60a5b5080b2478c53cffbdb082e7191fad5f
talitabac <talitabac@gmail.com>
2014-06-21 12:50:14 -0300

    script

--- data/dicionario.sh
+++ data/dicionario.sh
@@ -1,6 +1,5 @@
-# compilar e rodar passando parametros e salvar o resultado num arquivo
-$parameters = "#something_here"
-javac metci.jar $parameters
-$result > log.txt
+#!/bin/bash
 
+# execucao
 
+java main $1 $2 $3 $4
